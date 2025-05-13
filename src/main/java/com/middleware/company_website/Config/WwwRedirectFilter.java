03b54151165eb75bbd149dc50e5f4603854fe00b
sekhar <ksekahr@middlewaretalents.com>
2025-05-13 19:24:40 +0530

    Custom domain added

---
+++ src/main/java/com/middleware/company_website/Config/WwwRedirectFilter.java
@@ -0,0 +1,34 @@
+package com.middleware.company_website.Config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class WwwRedirectFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        String host = req.getHeader("host");
+
+        if ("talentflow-hr.com/".equalsIgnoreCase(host)) {
+            String redirectURL = "https://www.talentflow-hr.com" + req.getRequestURI();
+            if (req.getQueryString() != null) {
+                redirectURL += "?" + req.getQueryString();
+            }
+
+            res.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
+            res.setHeader("Location", redirectURL);
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+}
