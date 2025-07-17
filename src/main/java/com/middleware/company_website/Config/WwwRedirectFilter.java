6f448869fec3f75f08b91a46b100039333fd2daa
sekhar <ksekahr@middlewaretalents.com>
2025-07-17 17:46:18 +0530

    redirecting to www

--- src/main/java/com/middleware/company_website/Config/WwwRedirectFilter.java
+++ src/main/java/com/middleware/company_website/Config/WwwRedirectFilter.java
@@ -4,9 +4,12 @@ import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 
 import java.io.IOException;
 
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @Component
 public class WwwRedirectFilter implements Filter {
 
